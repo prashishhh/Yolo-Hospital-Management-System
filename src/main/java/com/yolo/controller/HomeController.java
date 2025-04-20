fe1c7d816dcae7f049ab2df566263ac60f4e512c
Girish-Raj-Thapa <girishrajthapa03033@gmail.com>
2025-04-20 13:12:09 +0545

    Added comment in HomeController

--- src/main/java/com/yolo/controller/HomeController.java
+++ src/main/java/com/yolo/controller/HomeController.java
@@ -8,25 +8,32 @@ import jakarta.servlet.http.HttpServletResponse;
 import java.io.IOException;
 
 /**
- * Servlet implementation class HomeController
+ * HomeController handles requests to the home page of the application.
+ * It maps to both "/home" and root ("/") URLs and forwards requests to the home.jsp view.
  */
 @WebServlet(asyncSupported = true, urlPatterns = { "/home", "/" })
 public class HomeController extends HttpServlet {
 	private static final long serialVersionUID = 1L;
        
-    /**
-     * @see HttpServlet#HttpServlet()
+	/**
+     * Default constructor for HomeController.
      */
     public HomeController() {
         super();
         // TODO Auto-generated constructor stub
     }
 
-	/**
-	 * @see HttpServlet#doGet(HttpServletRequest request, HttpServletResponse response)
-	 */
+    /**
+     * Handles HTTP GET requests and forwards them to the home.jsp page located
+     * in /WEB-INF/pages/.
+     *
+     * @param request  The HttpServletRequest object that contains the request the client made.
+     * @param response The HttpServletResponse object that contains the response the servlet returns.
+     * @throws ServletException if a servlet-specific error occurs
+     * @throws IOException      if an I/O error occurs
+     */
 	protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
-		// TODO Auto-generated method stub
+		
 		request.getRequestDispatcher("/WEB-INF/pages/home.jsp").forward(request, response);
 	}
 
